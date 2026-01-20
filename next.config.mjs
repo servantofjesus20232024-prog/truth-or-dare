dbb29880711c774794881e321f1846749d958409
lance PC <lanceaziz2002@gmail.com>
2026-01-20 04:26:12 +0200

    next js config for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: 'export',
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+
+  // Optional: Add a base path if you are deploying to a subdirectory (e.g. /repo-name)
+  // basePath: '/truth-or-dare',
+
   reactCompiler: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
